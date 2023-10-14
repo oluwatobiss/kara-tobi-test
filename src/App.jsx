649458adeb49d6b1fd6e0201171f76040b1e4160
test <karaguarraci@gmail.com>
2023-10-14 17:23:46 +0100

    feat: test box

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import "@vtmn/css-card/dist/index.css";
 
 function App() {
   return (
     <>
-      <div className="card"></div>
+      <div className="card">
+        <div className="image-container">
+          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574" />
+        </div>
+        <div className="content">
+          <p>Chorizo & mozzarella gnocchi bake</p>
+          <button type="button">More Info</button>
+        </div>
+      </div>
     </>
   );
 }
